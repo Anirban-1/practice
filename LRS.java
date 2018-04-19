4a5879f7ce7ec18acdc7e5e57a2290628e752b24
Kaival Shah <kaival.shah@amobee.com>
2018-04-19 11:04:13 +0530

    modified to dp approach to LRS

--- LRS.java
+++ LRS.java
@@ -8,21 +8,21 @@ import java.io.*;
 
 class LRS
 {
-	static String f(char[]ar,int i,int j){
-		if(i==ar.length || j==ar.length)return "";
-		if(ar[i]==ar[j] && i!=j)return ar[i]+f(ar,i+1,j+1);
-		else{
-			String suffixA=f(ar,i+1,j);
-			String suffixB=f(ar,i,j+1);
-			if(suffixA.length()>suffixB.length())
-				return suffixA;
-			return suffixB;
-		}
-	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		BufferedReader buff=new BufferedReader(new InputStreamReader(System.in));
 		char []ar=buff.readLine().toCharArray();
-		System.out.println(f(ar,0,0));
+		int n=ar.length+1;
+		int dp[][]=new int[n][n];	
+		for(int i=n-2;i>=0;i--){
+			for(int j=n-2;j>=i;j--){
+				if(ar[i]==ar[j] &&i!=j)dp[i][j]=1+dp[i+1][j+1];
+				else dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+			}
+		}
+		String res="";
+		for(int i=0;i<=n-2;i++)
+			if(dp[i][i]>dp[i+1][i+1])res+=ar[i];
+		System.out.println(res);
 	}
 }
