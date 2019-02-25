684539d47f070cb42eea38dcab5bb93718d14bbd
Anirban Acharya <acharya.anirban9@gmail.com>
2019-02-25 08:45:23 +0530

    Rectified Kth Smallest finding technique

--- FindKthSmallest.java
+++ FindKthSmallest.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.concurrent.*;
 
 class FindKthSmallest
 {
@@ -10,21 +11,26 @@ class FindKthSmallest
         ar[j]=temp;
     }
 	static int partition(int[]ar,int lo, int hi){
-		int i=lo,j=hi,key=ar[(hi+lo)/2];
+		int key=ar[lo];
+        int i=lo+1,j=hi;
 		while(true){
-            while(i<=j && ar[i]<=key)i++;
-            while(ar[j]>key)j--;
+            while(i<j&&ar[i]<key)i++;
+            while(i<=j && ar[j]>=key)j--;
             if(i>=j)break;
             swap(ar, i, j);
         }
+        swap(ar,lo,j);
 		return j;
     }
-    static int findK(int []ar,int lo, int hi, int k){
-        if(lo==hi && k==lo)return ar[k];
-        if(k>hi || k<lo)return -1;
-        int p=partition(ar, lo, hi);
-        if(k<=p)return ar[k];
-        return findK(ar, p+1, hi, k);
+    static void sort(int []ar,int lo, int hi, int k){
+        if(lo>=hi)return;
+        int x = ThreadLocalRandom.current().nextInt(lo,hi);
+        swap(ar,x,lo);
+        int p=partition(ar,lo,hi);
+        int leftLength = p-lo+1;
+        if(k>=leftLength)
+            sort(ar,p+1,hi,k-leftLength);
+        else sort(ar,lo,p-1,k);
     }
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -34,6 +40,7 @@ class FindKthSmallest
 		int n=in.length;
 		int[]ar=new int[n];
         for(int i=0;i<n;i++)ar[i]=Integer.parseInt(in[i]);
-        System.out.println(findK(ar, 0, n-1, k-1));
+        sort(ar,0,n-1,k);
+        System.out.println(ar[k-1]);
 	}
 }
